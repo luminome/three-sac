64f38e2d0f79d8fac857b4db7f2463c84587f68b
SAC <sac.673@gmail.com>
2023-03-05 16:41:22 +0100

    fixing for index.js [package index]

---
+++ index.js
@@ -0,0 +1,6 @@
+import {uiBasicLoader as loader}  from 'ui-basic-loader.js';
+import {default as scene} from 'ui-three-base.js';
+import elements from 'ui-three-elements.js';
+import * as util from 'ui-util.js';
+
+export {loader, scene, elements, util};
